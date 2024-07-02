82d1493dcd6e08daa6bc00639becd410aab363f2
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-02 00:34:19 -0300

    Agregando Middleware para validar proyecto existente

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -5,23 +5,15 @@ import Project from "../models/Project";
 
 export class TaskController {
     static createTask = async (req: Request, res: Response) => {
-        const { projectId } = req.params;
-
-        const project = await Project.findById(projectId);
-
-        if(!project){
-            const error = new Error(`Proyecto con id=${projectId} no encontrado`)
-            return res.status(404).json({error: error.message})
-        }
 
         try {
             const task = new Task(req.body);
-            task.project = project.id;
+            task.project = req.project.id;
 
-            project.tasks.push(task.id);
+            req.project.tasks.push(task.id);
 
             await task.save();
-            await project.save();
+            await req.project.save();
             res.json({"msg":"Tarea creada correctamente"})
         } catch (error) {
             console.log(colors.red(error))
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -3,6 +3,7 @@ import { body, param } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputErrors } from "../middleware/validation";
 import TaskController from "../controllers/TaskController";
+import { validateProjectExists } from "../middleware/project";
 
 const router = Router();
 
@@ -40,6 +41,6 @@ router.delete("/:id",
     ProjectController.deleteProjectById);
 
 
-router.post("/:projectId/task", TaskController.createTask)
+router.post("/:projectId/task", validateProjectExists, TaskController.createTask)
 
 export default router
