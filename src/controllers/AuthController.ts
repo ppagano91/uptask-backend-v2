618454903bbfeb3bf07c99b2ebc89da5e537977c
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-08 11:28:52 -0300

    Modelo Token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,8 @@ import type { Request, Response } from "express"
 
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
+import Token from "../models/Token";
+import { generateToken } from "../utils/token";
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
@@ -13,10 +15,19 @@ export class AuthController {
                 const error = new Error("Ya existe un usuario registrado con ese email");
                 return res.status(409).json({msg: error.message, error: true})
             }
+
+            // Crear Usuario
             const user = new User(req.body);
 
+            // Hash Password
             user.password = await hashPassword(password);
-            await user.save();
+
+            // Generar Token
+            const token = new Token();
+            token.token = generateToken();
+            token.user = user.id;
+
+            await Promise.allSettled([user.save(), token.save()]);
 
             res.json({msg: "Cuenta creada exitosamente. Revisa tu email para confirmar cuenta"});
         } catch (error) {
