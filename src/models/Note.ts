444e65798ef0e965fb11e892c5c72fd3aac596a4
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-15 22:45:02 -0300

    Nuevo Modelo Nota

---
+++ src/models/Note.ts
@@ -0,0 +1,32 @@
+import mongoose, {Schema, Document, Types} from "mongoose";
+
+export interface INote extends Document {
+    content: string,
+    createdBy: Types.ObjectId,
+    task: Types.ObjectId
+}
+
+const NoteSchema: Schema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        createdBy: {
+            types: Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        task: {
+            types: Types.ObjectId,
+            ref: "Task",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Note = mongoose.model<INote>("Note", NoteSchema);
+export default Note;
