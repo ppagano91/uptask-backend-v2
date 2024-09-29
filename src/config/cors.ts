576eb544d1a99085644b64a7b287218d2eac8ac5
Patricio Pagano <pagano.patricio@gmail.com>
2024-09-29 11:23:08 -0300

    Cambio en cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,16 +1,18 @@
-import { CorsOptions } from "cors";
-
+import { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+ 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback){
-        const whiteList = [process.env.FRONTEND_URL]
-
-        if(process.argv[2] == "--api"){
-            whiteList.push(undefined);
-        }
-        if(whiteList.includes(origin)){
-            callback(null, true)
-        } else {
-            callback(new Error("Error de CORS"))
-        }
+  origin: function (origin, callback) {
+    const whiteList = [process.env.FRONTEND_URL];
+ 
+    if (process.argv[2] == '--api') {
+      whiteList.push(undefined);
     }
-}
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  },
+};
