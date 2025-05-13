d402c57dd52831298334ffede114425e7ea8f115
Patricio Pagano <pagano.patricio@gmail.com>
2025-05-13 08:02:32 -0300

    Actualización cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,16 +2,14 @@ import { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const corsConfig: CorsOptions = {
+export const corsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
-    if (process.argv[2] == '--api') {
-      whiteList.push(undefined);
-    }
-    if (whiteList.includes(origin)) {
+    if (!origin || whiteList.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error(`Error de CORS -> origin: ${origin} - whitelist: ${process.env.FRONTEND_URL}`));
+      callback(new Error(`Error de CORS -> origin: ${origin} - whitelist: ${whiteList}`));
     }
   },
+  credentials: true,
 };
